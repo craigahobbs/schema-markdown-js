fba40aec1d36da655a120606a943b15247972ff3
Craig Hobbs <craigahobbs@gmail.com>
2023-04-14 08:09:21 -0700

    improve schema-markdown-mode.el

--- language/schema-markdown-mode.el
+++ language/schema-markdown-mode.el
@@ -1,6 +1,6 @@
-;;; schema-markdown-mode.el --- major mode for editing Schema Markdown
+;;; schema-markdown-mode.el --- Major mode for editing Schema Markdown files
 
-;; Version: 0.1
+;; Version: 0.2
 
 ;;; Commentary:
 
@@ -13,50 +13,69 @@
 ;;     (url-copy-file "https://craigahobbs.github.io/schema-markdown-js/language/schema-markdown-mode.el" mode-file t)
 ;;     (package-install-file mode-file)
 ;;     (delete-file mode-file)))
+;; (add-to-list 'auto-mode-alist '("\\.smd\\'" . schema-markdown-mode))
 
 ;;; Code:
-(require 'generic-x)
+
+(defconst schema-markdown-keywords
+  (regexp-opt
+   '("action" "enum" "errors" "group" "input" "nullable" "optional" "output"
+      "path" "query" "struct" "typedef" "union" "urls")
+   'words)
+  )
+
+(defconst schema-markdown-types
+  (regexp-opt
+   '("bool" "date" "datetime" "float" "int" "object" "string" "uuid")
+   'words)
+  )
+
+(defconst schema-markdown-font-lock-keywords
+  (list
+   (cons schema-markdown-keywords 'font-lock-keyword-face)
+   (cons schema-markdown-types 'font-lock-type-face)
+
+   ;; Rule for variable assignment highlighting
+   '("^\\s-*\\([_A-Za-z][_A-Za-z0-9]*\\)\\s-*=" 1 'font-lock-variable-name-face)
+
+   ;; Rule for label highlighting
+   '("^\\s-*\\([_A-Za-z][_A-Za-z0-9]*\\)\\s-*:\\s-*$" 1 'font-lock-constant-face)
+   )
+  )
+
+(defun schema-markdown-open-language-documentation ()
+  "Open Schema Markdown language documentation"
+  (interactive)
+  (browse-url "https://craigahobbs.github.io/schema-markdown-js/language/")
+  )
 
 ;;;###autoload
-(defun define-schema-markdown-mode()
-  (define-generic-mode 'schema-markdown-mode
-    '(?#)
-    '(
-      "action"
-      "enum"
-      "errors"
-      "group"
-      "input"
-      "nullable"
-      "optional"
-      "output"
-      "path"
-      "query"
-      "struct"
-      "typedef"
-      "union"
-      "urls"
-      )
-    (list
-     (cons
-      (regexp-opt
-       '(
-         "bool"
-         "date"
-         "datetime"
-         "float"
-         "int"
-         "object"
-         "string"
-         "uuid"
-         ) 'words) 'font-lock-type-face)
-     )
-    '("\\.smd\\'")
-    nil
-    "Major mode for editing Schema Markdown"))
+(define-derived-mode schema-markdown-mode prog-mode "Schema Markdown"
+  "Major mode for editing Schema Markdown files"
 
-;;;###autoload
-(define-schema-markdown-mode)
+  ; Ensure double quotes are treated as string delimiters
+  (modify-syntax-entry ?\" "\"" schema-markdown-mode-syntax-table)
+
+  ; Ensure backslashes are treated as escape characters
+  (modify-syntax-entry ?\\ "\\" schema-markdown-mode-syntax-table)
+
+  ; Specify that comments start with '#'
+  (modify-syntax-entry ?# "<" schema-markdown-mode-syntax-table)
+
+  ; Specify that comments end with a newline
+  (modify-syntax-entry ?\n ">" schema-markdown-mode-syntax-table)
+
+  ;; Set comment-related variables
+  (setq-local comment-start "#")
+  (setq-local comment-start-skip "#+\\s-*")
+
+  ;; Apply font-lock rules for syntax highlighting
+  (setq-local font-lock-defaults '(schema-markdown-font-lock-keywords))
+
+  ;; Bind the key for browsing documentation
+  (define-key schema-markdown-mode-map (kbd "C-c C-l") 'schema-markdown-open-language-documentation)
+  )
 
 (provide 'schema-markdown-mode)
+
 ;;; schema-markdown-mode.el ends here
